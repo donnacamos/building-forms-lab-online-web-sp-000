8d59cb9db4c2daf064a4c45e75f8a62a0f10a88f
Donna Amos <donnacamos.developer@gmail.com>
2019-12-18 19:36:29 +0000

    Automatically backed up by Learn

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,14 +1,12 @@
 export default function manageBand(state = {
   bands: [],
 }, action) {
-  switch(action.type){
+  switch (action.type) {
+    case 'ADD_BAND':
 
-    case "ADD_BAND":
-      return {
-        ...state,
-        bands: [...state.bands, action.band]
-      }
-    default:
-      return state
+    return { ...state, bands: [...state.bands, action.band] }
+
+  default:
+    return state;
   }
 };
